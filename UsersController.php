53ba2bb45682235abd2af45219ec4dbfbefeb8e1
dima-loburec <dima-loburec@yandex.ru>
2015-11-20 14:01:21 +0600

    added photo, birthday, gender and minor edits

--- UsersController.php
+++ UsersController.php
@@ -8,6 +8,8 @@ App::uses('AppController', 'Controller');
 
 class UsersController extends AppController {
     
+    var $uses = array('Event', 'Action', 'User', 'Voting', 'Subscription');
+    
     public function beforeFilter() {
         
         $this->Auth->allow('auth_vk', 'logout');
@@ -64,6 +66,7 @@ class UsersController extends AppController {
         }
         
         if ($token === false) {
+            
             $this->set('errorMessage', 'ВК не отвечает');
             
             return false;
@@ -72,6 +75,7 @@ class UsersController extends AppController {
         $token = json_decode($token, true);
  
         if (!isset($token['access_token'])) {
+            
             $this->set('errorMessage', 'ВК вас не авторизовал');
             
             return false;
@@ -79,7 +83,7 @@ class UsersController extends AppController {
         
         $params = array(
             'uids'         => $token['user_id'],
-            'fields'       => 'uid,first_name,last_name,sex,bdate',
+            'fields'       => 'uid,first_name,last_name,sex,bdate,photo_400_orig',
             'access_token' => $token['access_token']
         );
         $userInfoLink = Configure::read('vkAuth.urlGetUserInfo') 
@@ -94,6 +98,7 @@ class UsersController extends AppController {
         }
         
         if ($userInfo === false) {
+            
             $this->set('errorMessage', 'ВК не отвечает');
             
             return false;
@@ -102,6 +107,7 @@ class UsersController extends AppController {
         $userInfo = json_decode($userInfo, true);
         
         if (!isset($userInfo['response'][0]['uid'])) {
+            
             $this->set('errorMessage', 'ВК вас не авторизовал');
             
             return false;
@@ -110,12 +116,43 @@ class UsersController extends AppController {
         $userInfo = $userInfo['response'][0]; 
         
         if (isset($token['email'])) {
-            
             $userInfo['email'] = $token['email'];
-        
         } else {
             $userInfo['email'] = '';   
         }
+
+        if (isset($userInfo['photo_400_orig'])) {
+            $userInfo['photo'] = $userInfo['photo_400_orig'];
+            unset($userInfo['photo_400_orig']);
+        } else {
+            $userInfo['photo'] = '';   
+        }
+        
+        if (isset($userInfo['sex'])) {
+            
+            if ($userInfo['sex'] == 1) {
+                $userInfo['gender'] = 'w';
+            } elseif ($userInfo['sex'] == 2) {
+                $userInfo['gender'] = 'm';   
+            } else {
+                $userInfo['gender'] = '';
+            }
+            
+            unset($userInfo['sex']);
+        } else {
+            $userInfo['gender'] = '';   
+        } 
+
+        if (isset($userInfo['bdate'])) {
+            $pattern = "/^([0-9]+)\.([0-9]+)\.([0-9]+)$/";
+            if (preg_match($pattern, $userInfo['bdate'])) {
+                $date = date('Y-m-d', strtotime($userInfo['bdate']));
+                $userInfo['birthday'] = $date;
+            }
+            unset($userInfo['bdate']);
+        } else {
+            $userInfo['birthday'] = '';   
+        } 
         
         if ($this->Session->check('User.authReferLink')) {                    
             $referLink = $this->Session->read('User.authReferLink');  
@@ -127,12 +164,17 @@ class UsersController extends AppController {
         $this->addUser($userInfo, 'vk', $referLink);               
 	}
     /**
-     * Add new users and authorize their by id in social network 
+     * Add new users and authorize their by id in social network
+     *
+     * If id of social network is exist authorize user and update fields    
      *
      * array['uid'] int User id in social network
      * array['first_name'] string User first name
      * array['last_name'] string User last name
-     * array['email'] string User email
+     * array['email'] string User email or empty strint
+     * array['photo'] string Absolute link to user photo or empty string
+     * array['gender'] string User gender or empty string
+     * array['birthday'] string User birthday in format 'YYYY-MM-DD' ('Y-m-d' in date() function) or empty string
      *
      * @param array $userInfo Array user data (see above)
      * @param string $socialNetwork Designation of social network
@@ -146,7 +188,21 @@ class UsersController extends AppController {
      */     
     private function addUser($userInfo, $socialNetwork, $referLink, $role = 'user') {
         
-        $socialNetworkField = $socialNetwork . '_key';
+        $socialNetworkField = $socialNetwork . '_key';  
+
+        //fields that will be updated
+        $userActualFields = Array(
+            'User' => Array(
+                $socialNetworkField => $userInfo['uid'],
+                'first_name'        => $userInfo['first_name'],
+                'last_name'         => $userInfo['last_name'],
+                'email'             => $userInfo['email'],
+                'photo'             => $userInfo['photo'],
+                'birthday'          => $userInfo['birthday'],
+                'gender'            => $userInfo['gender']           
+            )
+        );
+
         $options = array(
             'conditions' => array(
                 'User.' . $socialNetworkField => $userInfo['uid']
@@ -156,23 +212,23 @@ class UsersController extends AppController {
         $savedUser = $this->User->find('first',  $options);                
         if (!empty($savedUser)) {
             
+            $this->User->id = $savedUser['User']['id'];
+            $this->User->save($userActualFields);
+            $savedUser = $this->User->find('first',  $options);
+            
             if ($this->Auth->login($savedUser['User'])) {
                 
                 $this->redirect($referLink);
                 
             }
         }
-        $userAr = Array(
-            'User' => Array(
-                $socialNetworkField => $userInfo['uid'],
-                'first_name'        => $userInfo['first_name'],
-                'last_name'         => $userInfo['last_name'],
-                'email'             => $userInfo['email'],
-                'role'              => $role               
-            )
-        );        
+        
+        //fields that filled only when creating new user
+        $userActualFields['User']['role'] = $role;
+        
         $this->User->create();
-        if ($this->User->save($userAr)) {
+        
+        if ($this->User->save($userActualFields)) {
             
             $savedUser = $this->User->find('first',  $options);
             if ($this->Auth->login($savedUser['User'])) {
